33dc7524ece8343aeaf166291ba35693963c9a7c
stevehari <66351224+stevehari@users.noreply.github.com>
2020-06-16 21:22:30 +0530

    Updates

---
+++ precache-manifest.a9bec5291359a0769bb5283a0bcfd88a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9200319c0619bb4b3c741f0edd9add48",
+    "url": "/friendslist/index.html"
+  },
+  {
+    "revision": "d4add6107f8fd2eb1c82",
+    "url": "/friendslist/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "deeeb4fc0cb2b966a8dc",
+    "url": "/friendslist/static/css/main.903767c4.chunk.css"
+  },
+  {
+    "revision": "d4add6107f8fd2eb1c82",
+    "url": "/friendslist/static/js/2.7ec5fd90.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/friendslist/static/js/2.7ec5fd90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "deeeb4fc0cb2b966a8dc",
+    "url": "/friendslist/static/js/main.2218e771.chunk.js"
+  },
+  {
+    "revision": "9592fed978aab1644b0e",
+    "url": "/friendslist/static/js/runtime-main.9f02ec68.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/friendslist/static/media/SEGA.12a3db8b.woff"
+  }
+]);
